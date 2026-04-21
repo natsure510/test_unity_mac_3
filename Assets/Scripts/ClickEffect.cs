a6a07ca42b94f9ea95a53bfd117ddea2f9f41a6d
natsure:mac <napnature8436@gmail.com>
2026-04-22 05:24:59 +0900

    fix: line type to multi line submit, add: click effect

---
+++ Assets/Scripts/ClickEffect.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickEffect : MonoBehaviour
+{
+    private Color color;
+    private SpriteRenderer sr;
+    void Start()
+    {
+        //  x, y, -5 に配置
+        //this.transform.Translate(new Vector3(0, 0, -5));
+        var tr = this.transform;
+        var pos = tr.position;
+        tr.position = new Vector3(pos.x, pos.y, -5);
+        sr = this.GetComponent<SpriteRenderer>();
+        color = sr.color;
+        Debug.Log($"pos => {this.transform.position}");
+    }
+
+    void Update()
+    {
+        // 徐々に薄くして消す
+        color.a -= 0.001f; // アルファ値を減少させる
+        //Debug.Log($"color.a => {color.a}");
+        sr.color = color;
+        if (color.a <= 0)
+        {
+            Destroy(this.gameObject); // アルファ値が0以下になったらオブジェクトを破壊
+            Debug.Log("Destroyed");
+        }
+    }
+}
--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -4,11 +4,14 @@ using UnityEngine.UI;
 public class GameDirector : MonoBehaviour
 {
     public Text Text;
+    [SerializeField] private GameObject effectsg;// prefab
+    [SerializeField] private Transform effectsg_Parent;
     private int clickCount = 0;
 
     void Start()
     {
         Text.text = "Click Count: " + clickCount;
+
     }
 
     public void USGClick()
@@ -16,5 +19,8 @@ public class GameDirector : MonoBehaviour
         clickCount++;
         Text.text = "Click Count: " + clickCount;
         Debug.Log("Clicked");
+
+        GameObject obj = Instantiate(effectsg, effectsg_Parent);
+        obj.transform.localPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 }
